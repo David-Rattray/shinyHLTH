251cd42b4da8ecd955d1c8b3720891a628c66a51
David-Rattray <dave.g.rattray@gmail.com>
2023-12-20 16:17:03 -0800

    added attach message
    Added a start up message to instruct users to run start_dashboard()

---
+++ R/zzz.R
@@ -0,0 +1,23 @@
+shinyHLTHStartupMessage <- function()
+{
+  # Startup message obtained as 
+  msg <- c(paste0(
+    packageVersion("shinyHLTH")),
+    "\nWelcome to the shinyHLTH package",
+    "n\Use start_dashboard() to begin creating a new dashboard")
+  return(msg)
+}
+
+.onAttach <- function(lib, pkg)
+{
+  # unlock .mclust variable allowing its modification
+  unlockBinding(".shinyHLTH", asNamespace("shinyHLTH")) 
+  # startup message
+  msg <- shinyHLTHStartupMessage()
+  if(!interactive())
+    msg[1] <- paste("Package 'shinyHLTH' version", packageVersion("shinyHLTH"))
+  packageStartupMessage(msg)      
+  invisible()
+}
+
+# reference https://stackoverflow.com/questions/67986577/how-to-create-custom-start-up-messages-for-r-packages
