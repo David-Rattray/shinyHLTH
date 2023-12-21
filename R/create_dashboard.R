85273e7220b99b46e313b1dcf3d57d74c314bfe5
David-Rattray <dave.g.rattray@gmail.com>
2023-12-21 09:23:13 -0800

    create_dashboard installs and works as expected
    Okay had to restart my R session to clear an error but so far so good. Now to review what it copies over.

--- R/create_dashboard.R
+++ R/create_dashboard.R
@@ -3,59 +3,38 @@
 #' Creates golem directory and header from template info
 #'
 #' @export
-create_dashboard <- function(path,...) {
+create_dashboard <- function(path) {
   # ensure path exists
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
-  file.copy( # copies shinyHLTH folder
+  # Copies basic file structure to path
+  file.copy(
     system.file("extdata/.", package = "shinyHLTH", mustWork = TRUE),
     path,
     recursive = TRUE
   )
 
+  # Create Rproject in the specified path
   # https://stackoverflow.com/questions/26126027/script-for-creating-a-new-project-in-rstudio
   usethis::create_project(path)
   
+  # Print message to console
   message("Adding RStudio project file to ", path)
   
-  dots <- list(...)
-
-  text_list <- list()
-
-  for (i in seq_along(dots)) {
-    text_list[[i]] <- dots[[i]]
-  }
-
-  names(text_list) <- names(dots)
-
+  pkg_name <- tail(unlist(strsplit(path, "/")), 1)
+  
+  # Create basic DESCRIPTION file
   contents <- c(
-    paste("Package:", text_list$pkg_name),
-    paste("Title:", text_list$title),
+    paste("Package:", pkg_name),
+    paste("Title:"),
     "Version: 0.0.0",
-    paste("Author:", text_list$username),
+    paste("Author:"),
     "Description: Your Description Here",
     "Encoding: UTF-8",
     "LazyData: true"
   )
-
+  
   writeLines(contents, con = file.path(path, "DESCRIPTION"))
-
-  url <- "https://www.rstudio.com/products/connect/"
-
-  # edit this for MoH / HSPP guidelines
-  header <- c(
-    "header <- function() {",
-    "\tdashboardHeader(",
-    "\t\ttitle = tagList(",
-    paste0("\t\t\tspan(class = 'logo-lg',a(\"", text_list$title, "\",style=\"color:white !important\",href='", url, "')),"),
-    paste0("\t\t\ta(style = \"margin: -15px\", href='", url ,"', img(src = \"www/HSIAR-Logo-1_big.png\", width=\"225%\"))"),
-    "\t\t),",
-    "\t\tcontrolbarIcon = \"gear\"",
-    "\t)",
-    "}"
-  )
-
-  writeLines(header, con = file.path(paste0(path, "/R"), "header.R"))
 
 }
 # 
